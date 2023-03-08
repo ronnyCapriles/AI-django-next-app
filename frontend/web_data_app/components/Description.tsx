092d6fca4590c1c4ce10f12394e94d8cee62a9d1
vladi2998 <vladimir.ac29@gmail.com>
2023-03-08 06:48:20 -0400

    chore: logo style

--- frontend/web_data_app/components/Description.tsx
+++ frontend/web_data_app/components/Description.tsx
@@ -1,7 +1,7 @@
 
 export default function Description (props:any){
     return (
-        <div className=" text-justify text-xs text-gris-descripcion  ">
+        <div className=" text-justify text-xs text-gris-descripcion ">
             <p> Restaurante de comida italiana para toda la familia desde 1955</p>
         </div>
     )
--- frontend/web_data_app/components/Logo.tsx
+++ frontend/web_data_app/components/Logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Logo (props:any) {
     const Logo = require ('../public/LogoComida.jpg')
     return(
-        <div className=" block bg-white  h-auto items-center justify-center p-4 rounded rounded-full w-2/6 shadow-logo">
-            <Image src={Logo} alt="LogoPrueba" className='h-25px w-25px' />
+        <div className=" block bg-white h-auto items-center justify-center p-4 rounded rounded-full w-2/6 shadow-logo">
+            <Image src={Logo} alt="LogoPrueba" className='h-25px w-25px rounded-full' />
         </div>
         
     )
