47ba14e71f84278457b983785980cec0407f4d2e
Cristian Marquina <15-10872@usb.ve>
2023-03-23 21:18:27 -0400

    feat: SwrProve metodo put

--- frontend/web_data_app/components/SwrProve.tsx
+++ frontend/web_data_app/components/SwrProve.tsx
@@ -1,13 +1,36 @@
 import useSWR from 'swr'
- 
-const fetcher = (args:any) => fetch(args).then(res => res.json())
+import axios from 'axios'
+// Funcion para get 
+const fetcher = async (url:any) => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Ha ocurrido un error: ${error}`);
+    }
+  }
+//const fetcher = (args:any) => fetch(args).then(res => res.json())
+// get post put delete path
+
+
+//Funcion para put
+
+const updateData = async (url:any, data:any) => {
+    try {
+        const response = await axios.put(url, data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 export default function SwrProve () {
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts/1', fetcher)
- 
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
- 
   // render data
-  return <div> {data.title}</div>
+  return <div> {data.id}</div>
+  
+  
 }
+
